6538b0e502856934a1bb285678cf73374c009ce1
Rayane <mustaphagameover111@gmail.com>
2024-09-19 12:13:43 +0100

    db script + class diagram

---
+++ Sql/db.sql
@@ -0,0 +1,51 @@
+CREATE TYPE Status AS ENUM ('ONGOING', 'DONE', 'CANCELLED');
+
+CREATE TABLE Customer (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR NOT NULL,
+                          address VARCHAR,
+                          phone VARCHAR,
+                          isProfessional BOOLEAN
+);
+
+CREATE TABLE Component (
+                           id SERIAL PRIMARY KEY,
+                           name VARCHAR(255) NOT NULL,
+                           vatRate DOUBLE PRECISION,
+);
+
+CREATE TABLE Material (
+                          unitCost DOUBLE PRECISION,
+                          quantity DOUBLE PRECISION,
+                          transportCost DOUBLE PRECISION,
+                          qualityCoefficient DOUBLE PRECISION
+) INHERITS (Component);
+
+CREATE TABLE Labor (
+                       hourlyRate DOUBLE PRECISION,
+                       workHours DOUBLE PRECISION,
+                       workerProductivity DOUBLE PRECISION
+) INHERITS (Component);
+
+CREATE TABLE Project (
+                         id SERIAL PRIMARY KEY,
+                         projectName VARCHAR NOT NULL,
+                         profitMargin DOUBLE PRECISION,
+                         totalCost DOUBLE PRECISION,
+                         projectStatus Status,
+                         customerId INTEGER REFERENCES Customer(id),
+                         componentId INTEGER REFERENCES Component(id)
+);
+
+CREATE TABLE Quote (
+                       id SERIAL PRIMARY KEY,
+                       estimatedAmount DOUBLE PRECISION,
+                       emissionDate DATE,
+                       validityDate DATE,
+                       accepted BOOLEAN,
+                       projectId INTEGER REFERENCES Project(id)
+);
+
+
+
+
